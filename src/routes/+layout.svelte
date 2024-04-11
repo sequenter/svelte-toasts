4954dad4490df4856536cea78b8869ac4fdef98d
sequenter <35529212+sequenter@users.noreply.github.com>
2024-04-11 03:15:11 +0100

    Updated metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,14 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>svelte-toasts | demo</title>
+	<meta
+		name="description"
+		content="A lightweight, functional and customisable Svelte toasts library"
+	/>
+</svelte:head>
+
 <main class="container w-full max-w-lg mx-auto leading-relaxed">
 	<slot />
 </main>
